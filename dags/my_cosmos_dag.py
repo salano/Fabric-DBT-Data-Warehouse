e7cfa024df44bf572d37d618dac12b4f27680d50
Cleveland Sullivan <salanosullivan@gmail.com>
2026-03-20 05:24:51 +0100

    format dag file

---
+++ dags/my_cosmos_dag.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from datetime import datetime
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from airflow import DAG
+
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "warehouse"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+profile_config = ProfileConfig(
+    profile_name="fabric_warehouse",
+    target_name="prod",
+    profiles_yml_filepath=DBT_ROOT_PATH / "profiles.yml",
+)
+
+dbt_fabric_dag = DbtDag(
+    project_config=ProjectConfig(DBT_ROOT_PATH,),
+    operator_args={"install_deps": True},
+    profile_config=profile_config,
+    schedule_interval="@daily",
+    start_date=datetime(2026, 3, 20),
+    catchup=False,
+    dag_id="dbt_fabric_dag",
+)
